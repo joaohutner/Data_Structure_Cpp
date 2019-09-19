c7ee8bc45e2e8306123198dbd490c09905aa367d
João Hütner <joaovictorhutner@hotmail.com>
2019-09-19 19:48:20 -0300

    update 19-09

--- Stack_LIFO/aberturas.cpp
+++ Stack_LIFO/aberturas.cpp
@@ -1,27 +1,31 @@
 #include <iostream>
-#include <locale.h>
 #include <string.h>
 using namespace std;
 #include "generic_stack.hpp"
 #include "especi_abert.hpp"
 
 int main(){
-    setlocale(LC_ALL, "Portuguese");
     TStack<TAnalise> aberturas;
     stack_start(aberturas);
+
+    TStack<TAnalise> fechaduras;
+    stack_start(fechaduras);
 
 
     TAnalise aux;
     char expre[100];
-    int i;
     char compare;
-    int contador = 0;
 
-    cout<<"Insira a sua expressão!"<<endl;
+    int i;
+    
+    int contador_abre = 0;
+    int contador_fecha = 0;
+
+    cout<<"Insira a sua expressao!"<<endl;
     cin>>expre;
 
     for(i=0;i<strlen(expre);i++){
-        if(expre[i] == '(' or expre[i] == '{' or expre[i] == '[' or expre[i] == ')' or expre[i] == '}' or expre[i] == ']'){
+        if(expre[i] == '(' or expre[i] == '{' or expre[i] == '['){
             if(expre[i] == '('){
                 aux.name = '(';
             }
@@ -31,7 +35,11 @@ int main(){
             else if(expre[i] == '{'){
                 aux.name = '{';
             }
-            else if(expre[i] == ')'){
+            stack_insert(aberturas,aux);
+            contador_abre++;
+        }
+        else if(expre[i] == ')' or expre[i] == '}' or expre[i] == ']'){
+            if(expre[i] == ')'){
                 aux.name = ')';
             }
             else if(expre[i] == ']'){
@@ -40,9 +48,48 @@ int main(){
             else if(expre[i] == '}'){
                 aux.name = '}';
             }
-            stack_insert(aberturas,aux);
-            contador++;
+            stack_insert(fechaduras,aux);
+            contador_fecha++;
         }
     }
+
+    int nope = 1;
+
+    char auxiliar_abre = ' ';
+    char auxilixar_fecha = ' ';
+
+
+    /*
+    if(contador_fecha==contador_abre){
+        for(i=0;i<contador_abre;i++){
+
+            auxiliar_abre=captura_(pop(aberturas));
+            auxilixar_fecha = captura_(pop(fechaduras));
+
+            if(auxiliar_abre=='(' and auxilixar_fecha==')'){
+                nope = 0;
+            }
+            else if(auxiliar_abre=='[' and auxilixar_fecha==']'){
+                nope = 0;
+            }
+            else if(auxiliar_abre=='{' and auxilixar_fecha=='}' ){
+                nope = 0;
+            }
+        }
+    } */
+    cout_(pop(aberturas));
+    cout_(pop(fechaduras));
+    
+    cout_(pop(aberturas));
+    cout_(pop(fechaduras));
+    
+    
+    if (nope == 1){
+        cout<<"\nEsta mal formado!";
+    }
+    else{
+        cout<<"\nEsta bem formado!";
+    }
+    
     return 0;
 }
--- Stack_LIFO/especi_abert.hpp
+++ Stack_LIFO/especi_abert.hpp
@@ -1,3 +1,11 @@
 struct TAnalise{
     char name;
 };
+
+void cout_(TAnalise a){
+    cout<<"Dado: "<<a.name<<endl;
+}
+
+char captura_(TAnalise a){
+    return a.name;
+}
